d3bd8b81e16134417648cb01c3bdc37935be6a17
Martin <mr.mmaurer+work@gmail.com>
2020-03-07 23:22:20 +0000

    Made runnable again

--- src/App.js
+++ src/App.js
@@ -28,6 +28,7 @@ function App() {
     const formIsReady = emailIsFilled && emailIsValid && passwordIsValid && passwordIsFilled;
 
     setFormIsSubmittable(formIsReady);
+  // eslint-disable-next-line
   }, [ email, password ]);
 
   const handleEmailChange = (event) => {
@@ -38,6 +39,7 @@ function App() {
 
     const emailIsNotEmpty = email.length > 0;
     // https://emailregex.com/
+    // eslint-disable-next-line
     const emailRegex = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
     const isValidEmail = emailRegex.test(value);
 
